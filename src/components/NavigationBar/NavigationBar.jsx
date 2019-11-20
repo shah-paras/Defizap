74527b1635c34b8b158b9b241560acba7786398d
Tosh-Sharma <toshkumar.s@somaiya.edu>
2019-11-20 10:40:41 +0530

    Navbar fixes and removing tawk.

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -8,97 +8,76 @@ import styles from './NavigationBar.module.css';
 
 const NavigationBar = props => {
   return (
-    <>
-      <Navbar
-        bg="primary"
-        variant="dark"
-        style={{ width: '200%', marginLeft: '-50%', justifyContent: 'center' }}
-      >
-        <Navbar.Brand className={styles.anchorTag}>
-          <span role="img">🏆</span>
-          <a
-            href="https://blog.kyber.network/kyberdefi-hackathon-meet-the-winners-bea5bc9ec983"
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: 'white', textDecoration: 'underline' }}
-            id="anchorTag"
-          >
-            DeFiZap wins #KyberDeFi Hackathon
-          </a>
-          <span role="img">🏆</span>
+    <div className="mt-1 mb-3">
+      <Navbar expand="md">
+        <Navbar.Brand href="/">
+          DeFiZap
+          <sup>Beta</sup>
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Navbar.Text>
+              <Button
+                className={styles.anchorSpacing}
+                variant="link"
+                href="/survey"
+              >
+                Get Started
+              </Button>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Button
+                className={styles.anchorSpacing}
+                variant="link"
+                href="/zaps"
+              >
+                Explore Zaps
+              </Button>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Button
+                variant="link"
+                href="/faq"
+                className={styles.anchorSpacing}
+              >
+                FAQ
+              </Button>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Button
+                href="https://devpost.com/software/defi-zap"
+                variant="link"
+                target="_blank"
+                className={styles.anchorSpacing}
+              >
+                Devpost
+              </Button>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Button
+                href="https://defitutorials.com/"
+                variant="link"
+                target="_blank"
+                className={styles.anchorSpacing}
+              >
+                Tutorials
+              </Button>
+            </Navbar.Text>
+            <Navbar.Text>
+              <Button
+                href="https://t.me/defizap"
+                variant="link"
+                target="_blank"
+                className={styles.anchorSpacing}
+              >
+                Telegram
+              </Button>
+            </Navbar.Text>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
-      <div className="mt-1 mb-3">
-        <Navbar expand="md">
-          <Navbar.Brand href="/">
-            DeFiZap
-            <sup>Beta</sup>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Navbar.Text>
-                <Button
-                  className={styles.anchorSpacing}
-                  variant="link"
-                  href="/survey"
-                >
-                  Get Started
-                </Button>
-              </Navbar.Text>
-              <Navbar.Text>
-                <Button
-                  className={styles.anchorSpacing}
-                  variant="link"
-                  href="/zaps"
-                >
-                  Explore Zaps
-                </Button>
-              </Navbar.Text>
-              <Navbar.Text>
-                <Button
-                  variant="link"
-                  href="/faq"
-                  className={styles.anchorSpacing}
-                >
-                  FAQ
-                </Button>
-              </Navbar.Text>
-              <Navbar.Text>
-                <Button
-                  href="https://devpost.com/software/defi-zap"
-                  variant="link"
-                  target="_blank"
-                  className={styles.anchorSpacing}
-                >
-                  Devpost
-                </Button>
-              </Navbar.Text>
-              <Navbar.Text>
-                <Button
-                  href="https://defitutorials.com/"
-                  variant="link"
-                  target="_blank"
-                  className={styles.anchorSpacing}
-                >
-                  Tutorials
-                </Button>
-              </Navbar.Text>
-              <Navbar.Text>
-                <Button
-                  href="https://t.me/defizap"
-                  variant="link"
-                  target="_blank"
-                  className={styles.anchorSpacing}
-                >
-                  Telegram
-                </Button>
-              </Navbar.Text>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    </>
+    </div>
   );
 };
 
