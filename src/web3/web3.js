d5a572362fc8b0d418627a878186b82c93532ce9
Tosh-Sharma <toshkumar.s@somaiya.edu>
2019-11-25 13:30:49 +0530

    Added fortmatic, porti, squarelink, infura and Fortmatic domain verification.

--- src/web3/web3.js
+++ src/web3/web3.js
@@ -4,6 +4,37 @@ import Web3 from 'web3';
 // eslint-disable-next-line import/no-mutable-exports
 let web3;
 
+const FORTMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY;
+const PORTIS_KEY = process.env.REACT_APP_PORTIS_KEY;
+const SQUARELINK_KEY = process.env.REACT_APP_SQUARELINK_KEY;
+const INFURA_KEY = process.env.REACT_APP_INFURA_KEY;
+
+const wallets = [
+  { walletName: 'coinbase', preferred: true },
+  { walletName: 'trust', preferred: true },
+  { walletName: 'metamask', preferred: true },
+  { walletName: 'dapper', preferred: true },
+  {
+    walletName: 'fortmatic',
+    apiKey: FORTMATIC_KEY,
+    preferred: true
+  },
+  {
+    walletName: 'portis',
+    apiKey: PORTIS_KEY,
+    preferred: true
+  },
+  {
+    walletName: 'squarelink',
+    apiKey: SQUARELINK_KEY
+  },
+  { walletName: 'authereum' },
+  {
+    walletName: 'walletConnect',
+    infuraKey: INFURA_KEY
+  }
+];
+
 const initializationOptions = {
   dappId: process.env.REACT_APP_BLOCKNATIVE_ID,
   networkId: 1,
@@ -12,6 +43,9 @@ const initializationOptions = {
     wallet: wallet => {
       web3 = new Web3(wallet.provider);
     }
+  },
+  walletSelect: {
+    wallets
   }
 };
 
