b3d0e4681d7b244aed86a4603b971ee07c0a63f9
Tosh-Sharma <toshkumar.s@somaiya.edu>
2019-12-23 23:00:23 +0530

    Updated the Zaps with the Gift a Zap feature.

--- src/components/Zaps/ZapFullViewV2.jsx
+++ src/components/Zaps/ZapFullViewV2.jsx
@@ -49,12 +49,12 @@ class ZapFullView extends PureComponent {
           </Row>
           <br />
           <Row className="d-flex justify-content-center align-items-center">
-            <Col xs={12} sm={4}>
+            <Col xs={12} sm={4} className="m-1">
               <BuyButtonContainer {...this.props} size="lg" />
             </Col>
-            {/* <Col xs={12} sm={4} className="justify-content-start">
-              <GiftButton {...this.props} />
-            </Col> */}
+            <Col xs={12} sm={4} className="justify-content-start m-1">
+              <GiftButton {...this.props} size="lg" />
+            </Col>
           </Row>
           <Row className="justify-content-center align-text-center">
             <Col xs={12} md={6} className="justify-content-center text-center">
